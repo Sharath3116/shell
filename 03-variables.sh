4efb5d1bc58bca5566de03a2c12aac8464c14fb6
Sharath3116 <sharath3192@gmail.com>
2025-04-28 18:50:47 +0530

    second script

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,7 +5,7 @@ echo "Hello Shell Script"
 PERSON1=Janshi
 Person2=Bheema
 
-echo $Person1:: Hello $person2, "Good Morning"
-echo $person2:: Tell me
-echo $Person1:: what are you up to, $Person2
-echo $person2:: whats your problem
+echo $Person1 :: Hello $person2, "Good Morning"
+echo $person2 :: Tell me
+echo $Person1 :: what are you up to, $Person2
+echo $person2 :: whats your problem
