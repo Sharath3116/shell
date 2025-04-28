65f77887bfef357867bdd85ac9f3ceb2cd69106b
Sharath3116 <sharath3192@gmail.com>
2025-04-28 15:00:14 +0530

    second script

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#1/bin/bash
+
+
+
+echo "ramehs":: hello sures, goodmorning:
+echo "ramehs" :: fuck you, ashole:
+echo "test" you ::all test , oo
+echo "hig" test :: ali, high:
+echo "sures" :: test "sures:
