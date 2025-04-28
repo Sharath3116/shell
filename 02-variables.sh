2f1de5e1a170eba868ad2d71ddca5daf245cc4e1
Sharath3116 <sharath3192@gmail.com>
2025-04-28 15:09:29 +0530

    second script

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,8 +1,9 @@
 #1/bin/bash
 
+PERSON1=Ramesh
+Person2=Suresh
 
-
-echo "ramehs":: hello sures, goodmorning:
+echo "$PERSON2":: hello sures, goodmorning:
 echo "ramehs" :: fuck you, ashole:
 echo "test" you ::all test , oo:
 echo "hig" test :: ali, high:
