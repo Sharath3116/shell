bdd0b24d71758bb3f723957be86c5c6ca073566e
Sharath3116 <sharath3192@gmail.com>
2025-04-29 23:02:47 +0530

    script update

--- 15-install-package.sh
+++ 15-install-package.sh
@@ -11,10 +11,13 @@ then
     else
         echo -e "your $G root user $N"
 fi
+
 yum install mysql -y
+
 if [ $? -eq 0 ]
+then
     echo -e "sql install $G SUCESS $N"
-    else 
+else 
     echo -e "sql install $R FAILURE $N"
     exit 1
 fi
