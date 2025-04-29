eeb28a28400c193eb2ded19b9fcad03ae9b283bd
Sharath3116 <sharath3192@gmail.com>
2025-04-29 08:30:25 +0530

    script update

--- 11-Date and time.sh
+++ 11-Date and time.sh
@@ -5,10 +5,13 @@ echo "script started at ${DATE}"
 
 PERSON1=$1
 PERSON2=$2
+NUMBER1=$3
+NUMBER2=$4
+SUM=$((NUMBER1+NUMBER2))
 
 echo "Hello my name is ${PERSON1}: have pet name ${PERSON2}"
-echo "what you expect me to do ${PERSON1}:nothing ${PERSON1}"
-
+echo "what you expect me to do ${PERSON2}:nothing whats is your problem ${PERSON1}"
+echo "Total is ${SUM}"
 
 DATE=$(TZ=IST-5:30 date +'%d/%m/%y %T %Z')
 echo "script ended at ${DATE}"
