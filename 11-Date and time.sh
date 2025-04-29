3f2401b05a9a37d44ca9b5b94e3644467e7a7bc4
Sharath3116 <sharath3192@gmail.com>
2025-04-29 08:18:49 +0530

    script update

--- 11-Date and time.sh
+++ 11-Date and time.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-DATE=$(date)
-
+DATE=$(TZ=IST-5:30 date +'%d/%m/%y %T %Z')
 echo "script started at ${DATE}"
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Hello my name is $PERSON1 :: i have pet name $PERSON2"
+echo "what you expect me to do $PERSON2 :: nothing $PERSON1"
+
+
+DATE=$(TZ=IST-5:30 date +'%d/%m/%y %T %Z')
+echo "script ended at ${DATE}"
+
+
+
+
+
+
 
