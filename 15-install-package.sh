c09a173c2967a37efeb40d25831a8f4fe724ca95
Sharath3116 <sharath3192@gmail.com>
2025-04-29 22:45:16 +0530

    script update

--- 15-install-package.sh
+++ 15-install-package.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+#!bin/bash
+
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[30m"
 
-if [ $ID -eq 0 ]
-then
-    echo -e "your $G root user $N"
-else
-    echo -e "your $R run script with root user $N"
+if [$ID -ne 0]
+then 
+    echo -e "run the script with $R root user $N"
     exit 1
+else 
+    echo -e "your $G root user $N"
+
 fi
+yum install mysql -y
+
+if [$? -ne 0]
+then 
+    echo -e "SQL install is $R failed $N"
+    exit 1
+else
+    echo -e "SQL install is $G Sucess $N"
+fi
+
