dd9318bb4a2574a26187f3d23bce4a386b30bb6c
Sharath3116 <sharath3192@gmail.com>
2025-04-29 15:48:29 +0530

    script update

--- 13-Refrashmnet.sh
+++ 13-Refrashmnet.sh
@@ -8,23 +8,21 @@ read
 ITEM1=$Tea
 ITEM2=$Coffe
 ITEM3=$Milk
-if 
+if ($read eq Tea)
 {
-    ($read eq Tea)
     echo " Tea cost is 10/-"
     }
-{
-else if
-        ($read eq Coffe)
-        echo "Coffe price is 15/-"
-}
-{
-else if 
-        ($read eq Milj)
-        echo "Milk price is 20/-"
-        }
-exit 0
 
+else if ($read eq Coffe)
+{
+     echo "Coffe price is 15/-"
+}
+
+else if ($read eq Milj)
+{
+        echo "Milk price is 20/-"
+}
+exit 0
 fi
 
 
