8b191ca3e2746b26a8577a26e2097c5d7c01f7fc
Sharath3116 <sharath3192@gmail.com>
2025-04-29 15:57:20 +0530

    script update

--- 13-Refrashmnet.sh
+++ 13-Refrashmnet.sh
@@ -4,21 +4,19 @@ echo "WeLcOmE to Janshi CaFe"
 echo "Following are the refrashment avalaible"
 echo "tea, milk, coffe"
 
-read
-ITEM1=$Tea
-ITEM2=$Coffe
-ITEM3=$Milk
-if ($read -eq $ITEM1)
+read iteam $i
+
+if ($i eq tea)
 {
-    echo " Tea cost is 10/-"
+    echo "Tea cost is 10/-"
     }
 
-else if ($read -eq $ITEM2)
+else if ($i -eq milk)
 {
      echo "Coffe price is 15/-"
 }
 
-else if ($read -eq $ITEM3)
+else if ($i -eq coffe)
 {
         echo "Milk price is 20/-"
 }
