ae3acecccf7d7ab29f7af300e852a7c1a385ea88
Sharath3116 <sharath3192@gmail.com>
2025-04-30 11:06:28 +0530

    Updated scripts

--- 17-new-script.sh
+++ 17-new-script.sh
@@ -2,9 +2,9 @@
 
 ID=$(id -u)
 
-SCRIPTNAME=$0
-TIMESTAMP=$(date +'%d/%m/%y %T %Z')
-LOGFILE="/tmp/$SCRIPTNAME-$TIMESTAMP.log"
+SCRIPTNAME=$(basename "$0")
+TIMESTAMP=$(date +'%d-%m-%Y_%H-%M-%S')
+LOGFILE="/tmp/${SCRIPTNAME}-${TIMESTAMP}.log"
 
 R="\e[31m"
 G="\e[32m"
@@ -15,9 +15,9 @@ then
     echo -e "Error:: run the script with $R root user $N"
     exit 1
 else
-    echo -e "$G your root user$N"
+    echo -e "$G your are root user$N"
 fi
-yum install mysql -y &>> "$LOGFILE"
+yum install mysql -y &>> $LOGFILE
 
 if [ $? -eq 0 ]
 then 
