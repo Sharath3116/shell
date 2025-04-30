32c152a110d38435731fc73b0c29742b90734156
Sharath3116 <sharath3192@gmail.com>
2025-04-30 15:25:55 +0530

    Updated scripts

--- 19-loops.sh
+++ 19-loops.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
     echo -e "Starting the Script $TIMESTAMP" &>> LOGFILE
-
+    echo -e "Starting the Script $TIMESTAMP"
 VALIDATE (){
              if [ $1 -ne 0 ]
             then 
