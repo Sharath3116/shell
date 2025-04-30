fb1b54880865496da4785a5541afee8933556161
Sharath3116 <sharath3192@gmail.com>
2025-04-30 10:31:44 +0530

    Updated scripts

--- 08-addition.sh
+++ 08-addition.sh
@@ -2,5 +2,10 @@
 
 NUMBER1=$1
 NUMBER2=$2
+
 SUM=$(($NUMBER1+$NUMBER2))
-echo "print:: $SUM"
+echo "Total is $SUM"
+
+echo "How many arg passed :: $#"
+echo "All args passwed :: $@"
+echo "The script name is $0"
