b3795ed5a22a3cf538767db382d0e7d813ed00e1
Sharath3116 <sharath3192@gmail.com>
2025-04-30 15:49:54 +0530

    Updated scripts

--- 19-loops.sh
+++ 19-loops.sh
@@ -10,7 +10,6 @@ Y="\e[33m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
     echo -e "Starting the Script $TIMESTAMP" &>> LOGFILE
-    echo -e "Starting the Script $TIMESTAMP"
 VALIDATE (){
              if [ $1 -ne 0 ]
             then 
@@ -31,10 +30,10 @@ fi
 
 for package in $@
 do
-    yum list installed $package
+    yum list installed $package &>> $LOGFILE
     if [ $? -ne 0 ]
     then
-        yum install $package -y
+        yum install $package -y &>> $LOGFILE
         VALIDATE $? "Installation of $package"
     else
         echo -e "$package is $Y allready installed $N"
