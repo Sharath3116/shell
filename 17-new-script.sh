409b9e29dc0da48bbdc2385c85433dd799bb365b
Sharath3116 <sharath3192@gmail.com>
2025-04-30 10:23:13 +0530

    Updated scripts

---
+++ 17-new-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bahs
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+#LOGFILE=
+
+if [ $ID -ne 0 ]
+then
+    echo -e "Error:: run the script with $R root user $N"
+    exit 1
+else
+    echo -e "$G your root user$N"
+fi
+yum install mysql -y &>> /temp/temp.log
+
+if [ $? -eq 0 ]
+then 
+    echo -e "SQL install is $G SUCESS $N"
+else    
+    echo -e "SQL install is $R Failed $N"
+fi
