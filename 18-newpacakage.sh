967d31c436e421c39a030d0be3795ca6c69e5a35
Sharath3116 <sharath3192@gmail.com>
2025-04-30 12:34:11 +0530

    Updated scripts

--- 18-newpacakage.sh
+++ 18-newpacakage.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE() {
-            if [ &? -ne 0 ]
+        if [ $1 -ne 0 ]
         then 
             echo -e "Pacakage installation$R FAILED $N"
             exit 1
